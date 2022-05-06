275a7ea18520df1a9563d14bbeb6d86515d824f2
Quentin Le Nézet <quentin.lenezet@epfl.ch>
2022-05-06 11:38:43 +0200

    Ajout des fichiers send pour plus de clairté

---
+++ projet/send.c
@@ -0,0 +1,27 @@
+#include <ch.h>
+#include <hal.h>
+#include <usbcfg.h>
+
+#include "send.h"
+
+// Fonctionn du TP4
+void SendUint8ToComputer(uint8_t* data, uint16_t size)
+{
+	chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)"START", 5);
+	chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)&size, sizeof(uint16_t));
+	chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)data, size);
+}
+
+// Fonction du TP4
+void serial_start(void)
+{
+	static SerialConfig ser_cfg = {
+	    115200,
+	    0,
+	    0,
+	    0,
+	};
+
+	sdStart(&SD3, &ser_cfg); // UART3.
+}
+
